a56f97e90e8b995fc8bec3232feed66c025fd728
Anna Raketa <rocketvsaliens@gmail.com>
2023-08-21 19:39:58 +0300

    Fix docstring in tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -5,14 +5,17 @@ from src.item import Item
 
 @pytest.fixture
 def item():
+    """Создаём тестовый экземпляр класса"""
     return Item("Тестовый товар", 10.0, 5)
 
 
 def test_calculate_total_price(item):
+    """Проверяем, что выводится общая стоимость товаров"""
     assert item.calculate_total_price() == 50.0
 
 
 def test_apply_discount(item):
+    """Проверяем, что устанавливается и применяется скидка на товар"""
     item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 8.0
