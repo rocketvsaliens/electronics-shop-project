c22d59f04bbc44e2a8c36785a4d736071f54c3bb
Anna Raketa <rocketvsaliens@gmail.com>
2023-08-23 23:13:15 +0300

    Add changes for homework2

--- src/item.py
+++ src/item.py
@@ -1,34 +1,70 @@
+import os
+import csv
+from math import floor
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+    MAX_LENGTH = 10
+    PATH_TO_CSV = os.path.join('src', 'items.csv')
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Загружает экземпляры класса из .csv
+        Если файла нет или он не читается, вызываем исключение FileNotFoundError
+        """
+        try:
+            with open(cls.PATH_TO_CSV, encoding='cp1251') as file:
+                reader = csv.DictReader(file)
+                for line in reader:
+                    item = cls(line['name'], float(line['price']), int(line['quantity']))
+                    cls.all.append(item)
+        except FileNotFoundError:
+            raise FileNotFoundError('Файл отсутствует или поверждён')
+
+    @staticmethod
+    def string_to_number(string):
+        """Переводит строку в целое число с округлением до меньшего значения"""
+        if isinstance(string, str):
+            try:
+                return floor(float(string))
+            except ValueError:
+                raise ValueError('Строка не является числом')
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
+        # self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) < self.MAX_LENGTH:
+            self.__name = name
+        else:
+            self.__name = name[:self.MAX_LENGTH]
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
-        total_price = 0
-        for item in self.all:
-            if item.name == self.name:
-                total_price += item.price * item.quantity
-        return total_price
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,6 @@
 import pytest
+import csv
+import os
 
 from src.item import Item
 
@@ -19,3 +21,76 @@ def test_apply_discount(item):
     item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 8.0
+
+
+def test_name_setter(item):
+    # Проверка установки корректного значения имени
+    item.name = "Кварк"
+    assert item.name == "Кварк"
+
+    # Проверка обрезки имени, если оно превышает максимальную длину
+    long_name = "Синхрофазатрон"
+    item.name = long_name
+    assert item.name == long_name[:Item.MAX_LENGTH]
+
+    # Проверка установки пустого имени
+    empty_name = ""
+    item.name = empty_name
+    assert item.name == empty_name
+
+    # Проверка установки имени, которое в точности равно максимальной длине
+    max_length_name = "A" * Item.MAX_LENGTH
+    item.name = max_length_name
+    assert item.name == max_length_name
+
+
+@pytest.mark.parametrize("string, expected_result", [
+    ("5", 5),
+    ("5.0", 5),
+    ("5.5", 5)
+])
+def test_string_to_number(string, expected_result):
+    """
+    Проверяем, что функция делает цифру из строки
+    и возвращает исключение, если
+    """
+    assert Item.string_to_number(string) == expected_result
+    with pytest.raises(ValueError):
+        string = "test_string"
+        Item.string_to_number(string)
+
+
+def test_instantiate_from_csv():
+    """
+    Функция создаёт временный .csv файл.
+    Проверяем корректность получения данных из .csv.
+    Проверяем вызов исключения, если файл отсутствует или повреждён.
+    """
+    data = [
+        {'name': 'item1', 'price': '10.0', 'quantity': '5'},
+        {'name': 'item2', 'price': '15.0', 'quantity': '3'},
+        {'name': 'item3', 'price': '20.0', 'quantity': '2'}
+    ]
+    with open('test.csv', 'w', encoding='cp1251', newline='') as file:
+        fieldnames = ['name', 'price', 'quantity']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+    # Вызываем метод instantiate_from_csv()
+    Item.PATH_TO_CSV = 'test.csv'
+    Item.all = []
+    Item.instantiate_from_csv()
+
+    # Проверяем результаты
+    assert len(Item.all) == 3
+    assert Item.all[0].name == 'item1'
+    assert Item.all[1].price == 15.0
+    assert Item.all[2].quantity == 2
+
+    # Удаляем временный .csv файл
+    os.remove('test.csv')
+
+    # Проверяем вызов исключения при отсутствии .csv файла
+    with pytest.raises(FileNotFoundError):
+        Item.instantiate_from_csv()
